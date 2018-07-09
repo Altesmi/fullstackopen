8d9a5623ceb8c14508d2e5e8bf4c4a6a2fbc876f
Olli-Pekka Tikkanen <optikkanen@gmail.com>
2018-07-09 15:33:48 +0300

    Tehtävä 1.3

--- osa1/tehtavat/src/index.js
+++ osa1/tehtavat/src/index.js
@@ -36,21 +36,30 @@ const Yhteensa = (props) => {
 
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = 'Reactin perusteet'
-    const tehtavia1 = 10
-    const osa2 = 'Tiedonvälitys propseilla'
-    const tehtavia2 = 7
-    const osa3 = 'Komponenttien tila'
-    const tehtavia3 = 14
+
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
 
     return (
 
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osa1={osa1} t1={tehtavia1} 
-            osa2={osa2} t2 = {tehtavia2}
-            osa3={osa3} t3 = {tehtavia3} />
-            <Yhteensa yht={tehtavia1+tehtavia2+tehtavia3} />
+            <Sisalto osa1={osa1.nimi} t1={osa1.tehtavia} 
+            osa2={osa2.nimi} t2 = {osa2.tehtavia}
+            osa3={osa3.nimi} t3 = {osa3.tehtavia} />
+            <Yhteensa yht={osa1.tehtavia+osa2.tehtavia+osa3.tehtavia} />
         </div>
 
     )
