db4e3e4748958cecc8d3728454c64dd71e43bc05
Olli-Pekka Tikkanen <optikkanen@gmail.com>
2018-10-25 20:32:33 +0300

    Tehtävä 4.1

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,44 +1,40 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require("http");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const Blog = mongoose.model('Blog', {
+const Blog = mongoose.model("Blog", {
   title: String,
   author: String,
   url: String,
   likes: Number
-})
+});
 
-module.exports = Blog
+module.exports = Blog;
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const mongoUrl = 'mongodb://testi:testi123@ds139193.mlab.com:39193/blogilista';
+mongoose.connect(mongoUrl);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs);
+  });
+});
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+  blog.save().then(result => {
+    response.status(201).json(result);
+  });
+});
 
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
