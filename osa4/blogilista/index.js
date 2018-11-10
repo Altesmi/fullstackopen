1d1f5f77a943b3540977e05677e8c889e0bbb55b
Olli-Pekka Tikkanen <optikkanen@gmail.com>
2018-11-10 21:39:27 +0200

    Tehtävä 4.3.

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -5,26 +5,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 
-// const mongoose = require("mongoose");
-
-// const Blog = mongoose.model("Blog", {
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number
-// });
-
-//module.exports = Blog;
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/blogs",blogsRouter)
-
-//require("dotenv").config();
-
-//mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-
-
 
 
 const PORT = 3003;
