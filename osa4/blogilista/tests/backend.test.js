48a81ca64fa4b99a2e87957a339d7d7b1b0130e1
Olli-Pekka Tikkanen <optikkanen@gmail.com>
2018-11-12 02:05:19 +0200

    Tehtävä 4.4

--- osa4/blogilista/tests/backend.test.js
+++ osa4/blogilista/tests/backend.test.js
@@ -6,3 +6,58 @@ test('dummy is called', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe.only('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'blogi 1',
+      author: 'Henkilo 2',
+      url: 'http://url1.com',
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  const listWitNoBlogs = []
+
+  const listWithOnlyOneDummyObject = [{}]
+
+  const listWithTwoBlogs = [{
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'blogi 1',
+    author: 'Henkilo 1',
+    url: 'http://url1.com',
+    likes: 2,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'blogi 1',
+    author: 'Henkilo 2',
+    url: 'http://url2.com',
+    likes: 12,
+    __v: 0
+  }
+  ]
+
+  test('when no blogs 0 is returned', () => {
+    const result = listHelper.totalLikes(listWitNoBlogs);
+    expect(result).toBe(0);
+  })
+
+  test('when dummy object is input 0 is returned', () => {
+    const result = listHelper.totalLikes(listWithOnlyOneDummyObject);
+    expect(result).toBe(0);
+  })
+
+  test('when one blog is input output is its likes', () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(2);
+  })
+
+  test('when 2 blogs are input output is the sum of likes', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs);
+    expect(result).toBe(14);
+  })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,8 +1,13 @@
 const dummy = (blogs) => {
     return 1;
   }
+
+const totalLikes = (blogs) => {
+  return (blogs.length>0 && Number.isInteger(blogs[0].likes))
+   ? blogs.reduce((sum, cur) => sum+cur.likes,0) : 0; 
+  }
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
   
