b69bb12c507320766d4b1dd09756ef14612b6d31
Olli-Pekka Tikkanen <optikkanen@gmail.com>
2018-11-13 00:19:02 +0200

    Tehtävät 4.5-4.7

--- osa4/blogilista/tests/backend.test.js
+++ osa4/blogilista/tests/backend.test.js
@@ -7,57 +7,209 @@ test('dummy is called', () => {
   expect(result).toBe(1)
 })
 
-describe.only('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'blogi 1',
-      author: 'Henkilo 2',
-      url: 'http://url1.com',
-      likes: 2,
-      __v: 0
-    }
-  ]
+describe('total likes', () => {
 
-  const listWitNoBlogs = []
 
-  const listWithOnlyOneDummyObject = [{}]
+  test('when no blogs 0 is returned', () => {
+    const result = listHelper.totalLikes(listWithNoBlogs)
+    expect(result).toBe(0)
+  })
 
-  const listWithTwoBlogs = [{
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'blogi 1',
-    author: 'Henkilo 1',
-    url: 'http://url1.com',
-    likes: 2,
-    __v: 0
-  },
+  test('when dummy object is input 0 is returned', () => {
+    const result = listHelper.totalLikes(listWithOnlyOneDummyObject)
+    expect(result).toBe(0)
+  })
+
+  test('when one blog is input output is its likes', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(2)
+  })
+
+  test('when 2 blogs are input output is the sum of likes', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs)
+    expect(result).toBe(14)
+  })
+})
+
+describe('favorite blog', () => {
+
+  test('when no blogs empty object is returned', () => {
+    const result = listHelper.favoriteBlog(listWithNoBlogs);
+    expect(result).toMatchObject({})
+  })
+
+  test('when dummy object is input empty object is returned',() => {
+    const result = listHelper.favoriteBlog(listWithOnlyOneDummyObject);
+    expect(result).toMatchObject({})
+  })
+
+  test('when 1 blog is input the same blog is returned', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toMatchObject(listWithOneBlog[0])
+  })
+
+  test('when multiple blogs are input the one with most likes is returned', () => {
+    const result = listHelper.favoriteBlog(listWithTwoBlogs);
+    expect(result).toMatchObject(listWithTwoBlogs[1])
+  })
+
+})
+
+describe('most blogs', () => {
+  test('when no blogs empty object is returned', () => {
+    const result = listHelper.mostBlogs(listWithNoBlogs);
+    expect(result).toMatchObject({})
+  })
+
+  test('when dummy object is input empty object is returned',() => {
+    const result = listHelper.mostBlogs(listWithOnlyOneDummyObject);
+    expect(result).toMatchObject({})
+  })
+
+  test('when 1 blog is input the author of the blog is returned with number of blogs', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog);
+    expect(result).toMatchObject({author: "Henkilo 2", blogs: 1})
+  })
+
+  test('when multiple blogs are input the name which has the highest blog count is returned together with the blog count', () => {
+    const result = listHelper.mostBlogs(listWithTenBlogs);
+    expect(result).toMatchObject({author: "Henkilo 3", blogs: 5})
+  })
+})
+
+describe.only('most blogs', () => {
+  test('when no blogs empty object is returned', () => {
+    const result = listHelper.mostLikes(listWithNoBlogs);
+    expect(result).toMatchObject({})
+  })
+
+  test('when dummy object is input empty object is returned',() => {
+    const result = listHelper.mostLikes(listWithOnlyOneDummyObject);
+    expect(result).toMatchObject({})
+  })
+
+  test('when 1 blog is input the author of the blog is returned with number of blogs', () => {
+    const result = listHelper.mostLikes(listWithOneBlog);
+    expect(result).toMatchObject({author: "Henkilo 2", likes: 2})
+  })
+
+  test('when multiple blogs are input the name which has the highest blog count is returned together with the blog count', () => {
+    const result = listHelper.mostLikes(listWithTenBlogs);
+    expect(result).toMatchObject({author: "Henkilo 3", likes: 40})
+  })
+})
+
+// Define some blog data
+const listWithOneBlog = [
   {
     _id: '5a422aa71b54a676234d17f8',
     title: 'blogi 1',
     author: 'Henkilo 2',
-    url: 'http://url2.com',
-    likes: 12,
+    url: 'http://url1.com',
+    likes: 2,
     __v: 0
   }
-  ]
+]
+const listWithNoBlogs = []
 
-  test('when no blogs 0 is returned', () => {
-    const result = listHelper.totalLikes(listWitNoBlogs);
-    expect(result).toBe(0);
-  })
+const listWithOnlyOneDummyObject = [{}]
 
-  test('when dummy object is input 0 is returned', () => {
-    const result = listHelper.totalLikes(listWithOnlyOneDummyObject);
-    expect(result).toBe(0);
-  })
+const listWithTwoBlogs = [{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 1',
+  url: 'http://url1.com',
+  likes: 2,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 2',
+  url: 'http://url2.com',
+  likes: 12,
+  __v: 0
+}
+]
 
-  test('when one blog is input output is its likes', () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    expect(result).toBe(2);
-  })
-
-  test('when 2 blogs are input output is the sum of likes', () => {
-    const result = listHelper.totalLikes(listWithTwoBlogs);
-    expect(result).toBe(14);
-  })
-})
+const listWithTenBlogs = [{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 1',
+  url: 'http://url1.com',
+  likes: 2,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 2',
+  url: 'http://url2.com',
+  likes: 12,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 1',
+  url: 'http://url1.com',
+  likes: 2,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 2',
+  url: 'http://url2.com',
+  likes: 12,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 1',
+  url: 'http://url1.com',
+  likes: 2,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 3',
+  url: 'http://url2.com',
+  likes: 12,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 3',
+  url: 'http://url1.com',
+  likes: 2,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 3',
+  url: 'http://url2.com',
+  likes: 12,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 3',
+  url: 'http://url1.com',
+  likes: 2,
+  __v: 0
+},
+{
+  _id: '5a422aa71b54a676234d17f8',
+  title: 'blogi 1',
+  author: 'Henkilo 3',
+  url: 'http://url2.com',
+  likes: 12,
+  __v: 0
+}
+]
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,13 +1,54 @@
 const dummy = (blogs) => {
-    return 1;
-  }
+  return 1;
+}
 
 const totalLikes = (blogs) => {
-  return (blogs.length>0 && Number.isInteger(blogs[0].likes))
-   ? blogs.reduce((sum, cur) => sum+cur.likes,0) : 0; 
-  }
+  return (blogs.length > 0 && Number.isInteger(blogs[0].likes))
+    ? blogs.reduce((sum, cur) => sum + cur.likes, 0) : 0;
+}
+
+const favoriteBlog = (blogs) => {
+  let favoriteBlog = blogs.find(blog => blog.likes === Math.max.apply(null, blogs.map(blog => blog.likes)))
+
+  return typeof(favoriteBlog) === 'undefined' ? {} : favoriteBlog
+}
+
+const mostBlogs = (blogs) => {
+
+  let counts = []; // initialize empty array
+  blogs.forEach(blog => { // goes trough every blog
+    if(counts.filter(count=>count.author===blog.author).length===0) { //if this name is not found in 'counts' count how many times this author is in blogs
   
-  module.exports = {
-    dummy, totalLikes
-  }
+      let tempArr = blogs.filter(b=>b.author===blog.author) //temporary array for storing all the object which have the name blog.name
+    
+      counts.push({author: blog.author, blogs: tempArr.length})
+      } else { // if the name is already in counts do not count again
+      }
+  })
+  // return the author with most blogs if multiple authors have the same amount of blogs the first entry is returned
+  return counts.length === 0 ? {} : counts.find(count => count.blogs === Math.max.apply(null,counts.map(count=>count.blogs)))
+}
+
+const mostLikes = (blogs) => { // This is pretty much the same function as listhelper.mostBlogs
+
+  let counts = []; 
+  blogs.forEach(blog => { 
+    if(counts.filter(count=>count.author===blog.author).length===0 && typeof(blog.author)!=='undefined') { 
   
+      let tempArr = blogs.filter(b=>b.author===blog.author)
+    
+      counts.push({author: blog.author, likes: tempArr.reduce((sum, cur) => sum + cur.likes, 0)})
+      } else { 
+      }
+  })
+  // return the author with most likes if multiple authors have the same amount of blogs the first entry is returned
+  return counts.length === 0 ? {} : counts.find(count => count.likes === Math.max.apply(null,counts.map(count=>count.likes)))
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
